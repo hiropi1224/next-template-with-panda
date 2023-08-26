f1503085588c07936a0a0d7336ef3be2356e72cc
Shirahamah <hiropi1224s@gmail.com>
2023-08-26 14:02:42 +0900

    🎉

---
+++ src/app/page.tsx
@@ -0,0 +1,14 @@
+import { css } from '@panda/css';
+
+export default function Home() {
+  return (
+    <div
+      className={css({
+        fontSize: '2xl',
+        fontWeight: 'bold',
+      })}
+    >
+      Hello 🐼!
+    </div>
+  );
+}
